3a4a27514e181d6a110dcf36db180fdf2729a72f
MarcoFarinetti <farinetti94@gmail.com>
2020-01-25 19:14:52 +0100

    map tab, center on current location

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCH47nqlht03t0Kql5-7wLz1qTn6NsKmvo")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,14 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Map extends StatefulWidget {
   @override
   MapState createState() => MapState();
 }
 
-class MapState extends State<Map>{
+class MapState extends State<Map> {
+  GoogleMapController mapController;
+
+  Position _position;
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  void initState() {
+    Geolocator().getLastKnownPosition().then((lastLocation) {
+      if (lastLocation != null) {
+        setState(() {
+          _position = new Position(
+              latitude: lastLocation.latitude,
+              longitude: lastLocation.longitude);
+        });
+      } else {
+        Geolocator()
+            .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+            .then((currentLocation) {
+          setState(() {
+            _position = new Position(
+                latitude: currentLocation.latitude,
+                longitude: currentLocation.longitude);
+          });
+        });
+      }
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.green,);
+    if (_position == null) {
+      return new Container();
+    }
+    return GoogleMap(
+      onMapCreated: _onMapCreated,
+      myLocationEnabled: true,
+      myLocationButtonEnabled: true,
+      initialCameraPosition: CameraPosition(
+          target: LatLng(_position.latitude, _position.longitude), zoom: 11.0),
+      onLongPress: createEvent,
+    );
+  }
+
+  void createEvent(LatLng latLng) {
+    showDialog(context: context, child: SimpleDialog(title: Text('Ciao'),));
   }
 
 }
