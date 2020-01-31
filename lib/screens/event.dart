d6c9616c6a44ab8428df512a108ee16e65f482da
MarcoFarinetti <farinetti94@gmail.com>
2020-01-31 22:56:03 +0100

    create event pop-up on long press

---
+++ lib/screens/event.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Event extends StatelessWidget {
+  const Event({Key key, this.latLng}) : super(key: key);
+
+  final LatLng latLng;
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -8,6 +8,7 @@ class Map extends StatefulWidget {
 }
 
 class MapState extends State<Map> with AutomaticKeepAliveClientMixin {
+
   GoogleMapController _mapController;
 
   void _onMapCreated(GoogleMapController controller) {
@@ -15,28 +16,51 @@ class MapState extends State<Map> with AutomaticKeepAliveClientMixin {
     Geolocator()
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
         .then((currentLocation) {
-      _mapController.moveCamera(CameraUpdate.newLatLng(LatLng(currentLocation.latitude, currentLocation.longitude)));
+      _mapController.moveCamera(CameraUpdate.newLatLng(
+          LatLng(currentLocation.latitude, currentLocation.longitude)));
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      onMapCreated: _onMapCreated,
-      myLocationEnabled: true,
-      myLocationButtonEnabled: true,
-      initialCameraPosition: CameraPosition(
-          target: LatLng(0.0, 0.0), zoom: 11.0),
-      onLongPress: createEvent,
-    );
+    super.build(context);
+    return GestureDetector(
+        onLongPressStart: createEvent,
+        child: GoogleMap(
+          onMapCreated: _onMapCreated,
+          myLocationEnabled: true,
+          myLocationButtonEnabled: true,
+          initialCameraPosition:
+              CameraPosition(target: LatLng(0.0, 0.0), zoom: 11.0),
+        ));
   }
 
-  void createEvent(LatLng latLng) {
-    showDialog(context: context, child: SimpleDialog(title: Text('Ciao'),));
+  void createEvent(LongPressStartDetails details) {
+    showMenu(
+        context: context,
+        elevation: 10.0,
+        items: <PopupMenuEntry>[
+          PopupMenuItem(
+            value: 0,
+            child: Row(
+              verticalDirection: VerticalDirection.up,
+              children: <Widget>[
+                Icon(Icons.event),
+                Text("Crea evento"),
+              ],
+            ),
+          )
+        ],
+        position: RelativeRect.fromRect(
+            details.globalPosition & Size(0, 0), // smaller rect, the touch area
+            Offset.zero &
+                Size(
+                    MediaQuery.of(context).size.width,
+                    MediaQuery.of(context).size.height) // Bigger rect, the entire screen
+            ));
   }
 
   @override
-  bool get wantKeepAlive => true;
-
+  bool get wantKeepAlive => false;
 
 }
